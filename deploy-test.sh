9a7f07a65e6ce18997a88f9668e79cac5ac0f543
Naga Chaitanya Atchula <nagachaitanyaatchula@gmail.com>
2022-10-30 14:13:31 -0400

    yes

--- deploy-test.sh
+++ deploy-test.sh
@@ -5,6 +5,6 @@ sh 'aws s3 ls'
 echo $1
 sh 'aws configure set aws_access_key_id $1'
 sh 'aws configure set aws_secret_access_key $2'
-//  sh 'aws configure set region us-east-1' 
-sh 'aws s3 cp bermtec-0.0.1.zip s3://bermtec228/lambda-test/'
+//  sh 'aws configure set region us-east-2' 
+sh 'aws s3 cp bermtec-0.0.1.zip s3://aws-bucket-s/lambda-test/'
 echo "Stage 2 Yes"
